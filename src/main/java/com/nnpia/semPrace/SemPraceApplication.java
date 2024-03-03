fe44d13a8c95dd4cea13c6c02e3784d80e98545b
petrj99 <100368883+petrj99@users.noreply.github.com>
2024-03-03 14:15:22 +0100

    Initial Commit

---
+++ src/main/java/com/nnpia/semPrace/SemPraceApplication.java
@@ -0,0 +1,13 @@
+package com.nnpia.semPrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemPraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemPraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnpia/semPrace/SemPraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnpia.semPrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemPraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
