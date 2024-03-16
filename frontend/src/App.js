2d8617c61aa65a96bef38c0996c9a79a9b8424f9
Petr Jiruše <petrjiruse@seznam.cz>
2024-03-16 17:15:14 +0100

    Add page routing library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
+import VehicleOffer from './VehicleOffer';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" exact element={<HomePage />} />
+              <Route path="/nabidka-vozidel" exact element={<VehicleOffer />} />
+          </Routes>
+      </BrowserRouter>
   );
 }
 
